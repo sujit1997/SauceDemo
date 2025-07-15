17d50aa14434a9b528bc4fa21c4dd3798efcc008
SUJIT <sujitmanmode1997@gmail.com>
2025-07-15 17:37:59 +0530

    first commit

--- src/main/java/UI_SELENIIUM_TESTNG/login.java
+++ src/main/java/UI_SELENIIUM_TESTNG/login.java
@@ -2,18 +2,32 @@ package UI_SELENIIUM_TESTNG;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class login {
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
 
-        ChromeDriver driver = new ChromeDriver();
+        public static String browser = "edgedriver"; //External configuration
+        public static WebDriver driver;
+    public static void main(String[] args) {
+        if(browser.equals("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+             driver = new FirefoxDriver();
+        } else if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+             driver = new ChromeDriver();
+        } else if (browser.equals("edgedriver")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/v1/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-        driver.close();
+        //driver.close();
     }
 }
